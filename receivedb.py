645351ecb98246b05af240840bc2d5ebe8d027ee
snehaljadhav7 <ssj1006@wildcats.unh.edu>
2019-12-10 18:59:09 -0500

    Adding changes

--- receivedb.py
+++ receivedb.py
@@ -12,24 +12,20 @@ channel.queue_declare(queue='leadsdb_high_priority')
 
 
 def callback_leads(ch, method, properties, body):
-    print(" [x] Received leads %r" % body)
-
+    # print(" [x] Received leads %r" % body)
     item = binary_to_json(body)
-
-    id = item["id"]
-    first_name = item["first_name"]
-    last_name =  item["last_name"]
-    email = item["gender"]
-    ip_address = item["ip_address"]
-    cc = item[cc]
-    country = item["country"]
-    birthdate = item["birthdate"]
-    salary = item["salary"]
-    title = item["title"]
-    insertVariblesIntoTable(id, first_name, last_name, email, gender, ip_address, cc, country, birthdate, salary, title)
+    connect(item, high_priority=False)
 
 
-def insertVariblesIntoTable(Id, First_name, Last_name, Email, Gender, Ip_address, CC, Country, Birthdate, Salary, Title):
+
+def callback_high_priority(ch, method, properties, body):
+    # print(" [x] Received high priority %r" % body)
+    item = binary_to_json(body)
+    connect(item, high_priority=True)
+
+
+
+def connect(item, high_priority):
     """ Connect to the PostgreSQL database server """
     conn = None
     try:
@@ -42,16 +38,38 @@ def insertVariblesIntoTable(Id, First_name, Last_name, Email, Gender, Ip_address
 
         # create a cursor
         cur = conn.cursor()
+        id = item["id"]
+        first_name = item["first_name"]
+        last_name =  item["last_name"]
+        email = item["email"]
+        gender = item["gender"]
+        ip_address = item["ip_address"]
+        cc = item["cc"]
+        country = item["country"]
+        birthdate = item["birthdate"]
+        salary = item["salary"]
+        title = item["title"]
 
-        # execute a statement
-        my_sql = 'INSERT INTO leads(id, first_name, last_name, email, gender, ip_address, cc, country, birthdate, salary, title) VALUES(%s, %s, %s, %s,%s, %s, %s, %s,%s, %s, %s)'
-        record_tuple = (Id, First_name, Last_name, Email, Gender, Ip_address, CC, Country, Birthdate, Salary, Title)
-        print('PostgreSQL database version:')
-        cur.execute('SELECT version()')
-        cur.execute(my_sql, record_tuple)
+        if birthdate == '':
+            birthdate = '01-01-2018'
+        if salary == '':
+            salary = 0
+        else:
+            salary = salary.replace("$",'')
+            salary = float(salary)
+        if cc == '':
+            cc = 0
+
+        #insertVariblesIntoTable(id, first_name, last_name, email, gender, ip_address, cc, country, birthdate, salary, title)
+        my_sql_leads = 'INSERT INTO leads(id, first_name, last_name, email, gender, ip_address, cc, country, birthdate, salary, title) VALUES(%s, %s, %s, %s,%s, %s, %s, %s,%s, %s, %s)'
+        my_sql_high_priority = 'INSERT INTO high_priority(id, first_name, last_name, email, gender, ip_address, cc, country, birthdate, salary, title) VALUES(%s, %s, %s, %s,%s, %s, %s, %s,%s, %s, %s)'
+        record_tuple = (id, first_name, last_name, email, gender, ip_address, cc, country, birthdate, salary, title)
+        if high_priority:
+            cur.execute(my_sql_high_priority, record_tuple)
+        else:
+            cur.execute(my_sql_leads, record_tuple)
         conn.commit()
         print("Record inserted successfully into Leads table")
-        # display the PostgreSQL database server version
         db_version = cur.fetchone()
         print(db_version)
 
@@ -65,14 +83,10 @@ def insertVariblesIntoTable(Id, First_name, Last_name, Email, Gender, Ip_address
             print('Database connection closed.')
 
 
-if __name__ == '__main__':
-    connect()
+#def insertVariblesIntoTable(Id, First_name, Last_name, Email, Gender, Ip_address, CC, Country, Birthdate, Salary, Title):
 
 
-def callback_high_priority(ch, method, properties, body):
-    print(" [x] Received high priority %r" % body)
-    item = binary_to_json(body)
-    print(item["first_name"])
+
 
 
 
